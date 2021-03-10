bfd4d2833280642b43fd1bdb44ce45b85940004d
Dario Castromonte <dario.castromonte@gmail.com>
2021-03-10 19:21:11 -0300

    commit para el heroku

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
+
 const express = require('express');
 
+
 const app = express();
+
+const port = process.env.PORT || 3000;
 
 app.get('/',function(req,res){
     res.send('Hola Mundo')
