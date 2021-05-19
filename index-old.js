ff35f2425683110566808e36330494fdddf04021
Dario Castromonte <dario.castromonte@gmail.com>
2021-05-19 01:56:51 -0300

    Added Comments

--- index-old.js
+++ index-old.js
@@ -1,7 +1,7 @@
 const http = require('http')
 
 const port = 3000;
-
+//createServer recibe una función Callback con 2 parametros, request y response. Requeste se refiere a las peticiones que le haremos al servidor y Response la respuesta del servidor.
 http.createServer((req, res)=>{
 
     console.log(req);
@@ -21,8 +21,9 @@ http.createServer((req, res)=>{
     res.write('id,name\n123,Dario\n456,Marta');
 */
     res.end();
-
+    //Aca le añadimos el puerto al final donde va a estar esperando la respuesta
 }).listen(port, () => {
     console.log(`Server started - Port ${port}`)
 });
 
+//El navegador solo nos hace peticiones GET necesitamos Postman para las otras peticiones
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
+//Primero se requiere express
 const express = require('express');
+//Segundo se crea el entry point osea app donde se ejecutara express()
 const app = express();
+//Especifico el puerto donde trabajar 
 const port = process.env.PORT || 3000;
+//Aca manipulo los servicios, es decir decido que hacer con la peticion que recibo e incluso puedo tener distintos path como /usuarios , etc. express posee todos los metodos de peticiones http osea, get, post, delelte, put, patch , etc. 
+
+//Con app.use puedo abrir elementos estaticos, como imagenes, html o css, en este caso va a ser lo primero en abrirse y no el path de .get /  
+app.use(express.static(__dirname + '/public'));
 
 app.get('/',function(req,res){
     res.send('Hola Mundo')
@@ -15,8 +22,10 @@ app.post('/',function(req,res){
     res.send('Post del path /')
 });
 
+// Nos da un status de si inicio el Servidor 
 app.listen(port, () => {
     console.log(`Server started - Port ${port}`);
 });
 
+//En el archivo .env dejamos las constantes de Enviroment o entorno de desarrollo las cuales son unicas del enterno en el que nos manejamos
 
